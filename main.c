31ef1ad00d6cad591032a60ebd547d3796c79f99
RPi2User <florian@hatzfeld.biz>
2025-08-20 23:01:19 +0200

    Initial Commit

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+int main() {
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in addr = {0};
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(8080);
+
+    bind(server_fd, (struct sockaddr*)&addr, sizeof(addr));
+    listen(server_fd, 1);
+    printf("Listening on port 8080...\n");
+
+    while (1) {
+        int client_fd = accept(server_fd, NULL, NULL);
+
+        // HTTP-Header und Body
+        const char *http_response =
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/plain\r\n"
+            "Content-Length: 6\r\n"
+            "\r\n"
+            "mlem :3";
+
+        send(client_fd, http_response, strlen(http_response), 0);
+        close(client_fd);
+    }
+    close(server_fd);
+    return 0;
+}
